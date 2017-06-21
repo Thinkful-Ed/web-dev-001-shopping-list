d7316c2ee0a0fd3f2b93b500ea0f32b108121cec
Benjamin White <benjamin.e.white1@gmail.com>
2017-06-21 10:33:23 -0400

    check items working

--- index.js
+++ index.js
@@ -12,16 +12,14 @@ const NEW_ITEM_FORM_ID = "#js-shopping-list-form";
 const NEW_ITEM_FORM_INPUT_CLASS = ".js-shopping-list-entry";
 const SHOPPING_LIST_ELEMENT_CLASS = ".js-shopping-list";
 const ITEM_CHECKED_TARGET_IDENTIFIER = "js-shopping-item";
-const ITEM_CHECKED_BUTTON_IDENTIFIER = "js-item-toggle";
 const ITEM_CHECKED_CLASS_NAME = "shopping-item__checked";
 const ITEM_INDEX_ATTRIBUTE  = "data-item-index";
 const ITEM_INDEX_ELEMENT_IDENTIFIER = "js-item-index-element";
 
 
-// used to point to element that users click to delete
-// an item from shopping list.
-const ITEM_DELETE_BUTTON_IDENTIFIER = "js-item-delete";
-
+// points to the DOM element that users click to
+// check/uncheck list items
+const ITEM_CHECKED_BUTTON_IDENTIFIER = "js-item-toggle";
 
 
 function generateItemElement(item, itemIndex, template) {
@@ -62,26 +60,6 @@ function addItemToShoppingList(itemName) {
 }
 
 
-// name says it all. responsible for deleting a list item.
-function deleteListItem(itemIndex) {
-  console.log(`Deleting item at index "${itemIndex}" from shopping list`);
-  // as with `addItemToShoppingLIst`, this function also has the side effect of
-  // mutating the global STORE value.
-  //
-  // we call `.splice` at the index of the list item we want to remove, with a length
-  // of 1. this has the effect of removing the desired item, and shifting all of the
-  // elements to the right of `itemIndex` (if any) over one place to the left, so we
-  // don't have an empty space in our list.
-  STORE.splice(itemIndex, 1);
-}
-
-
-
-function toggleCheckedForListItem(itemIndex) {
-  console.log(`Toggling checked property for item at index ${itemIndex}`);
-  STORE[itemIndex].checked = !STORE[itemIndex].checked;
-}
-
 
 function handleNewItemSubmit() {
   $(NEW_ITEM_FORM_ID).submit(function(event) {
@@ -94,38 +72,51 @@ function handleNewItemSubmit() {
   });
 }
 
-
+// this function is responsible for retieving the array index of a
+// shopping list item in the DOM. recall that we're storing this value
+// in a `data-item-index` attribute on each list item element in the DOM.
 function getItemIndexFromElement(item) {
   const itemIndexString = $(item)
     .closest(`.${ITEM_INDEX_ELEMENT_IDENTIFIER}`)
     .attr(ITEM_INDEX_ATTRIBUTE);
+  // the value of `data-item-index` will be a string, so we need to convert
+  // it to an integer, using the built-in JavaScript `parseInt` function.
   return parseInt(itemIndexString, 10);
 }
 
+// this function is reponsible for toggling the `checked` attribute on an item.
+function toggleCheckedForListItem(itemIndex) {
+  console.log(`Toggling checked property for item at index ${itemIndex}`);
+  // if `checked` was true, it becomes false, and vice-versa. also, here again
+  // we're relying on side effect / mutating the global `STORE`
+  STORE[itemIndex].checked = !STORE[itemIndex].checked;
+}
 
+
+
+// this function is responsible for noticing when users click the "checked" button
+// for a shopping list item. when that happens it toggles the checked styling for that
+// item.
 function handleItemCheckClicked() {
+  // note that we have to use event delegation here because list items are not originally
+  // in the DOM on page load.
   $(SHOPPING_LIST_ELEMENT_CLASS).on("click", `.${ITEM_CHECKED_BUTTON_IDENTIFIER}`, event => {
+    // call the `getItemIndexFromElement` function just above on the target of
+    // the current, clicked element in order to get the index of the clicked
+    // item in `STORE`
     const itemIndex = getItemIndexFromElement(event.currentTarget);
+    // toggle the clicked item's checked attribute
     toggleCheckedForListItem(itemIndex);
+    // render the updated shopping list
     renderShoppingList();
   });
 }
 
 
-// this function is responsible for noticing when users click the "Delete" button for
-// a shopping list item. when that happens, it removes the item from the shopping list
-// and then rerenders the updated shopping list.
 function handleDeleteItemClicked() {
-  // like in `handleItemCheckClicked`, we use event delegation
-
-  $(SHOPPING_LIST_ELEMENT_CLASS).on("click", `.${ITEM_DELETE_BUTTON_IDENTIFIER}`, event => {
-    // get the index of the item in STORE
-    const itemIndex = getItemIndexFromElement(event.currentTarget);
-    // delete the item
-    deleteListItem(itemIndex);
-    // render the updated shopping list
-    renderShoppingList();
-  });
+  // this function will be responsible for when users want to delete a shopping list
+  // item
+  console.log('`handleDeleteItemClicked` ran');
 }
 
 
