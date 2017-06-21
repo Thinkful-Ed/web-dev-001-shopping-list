0f2c39a6337d3f239cf8646e72aceeefecd7ee94
Benjamin White <benjamin.e.white1@gmail.com>
2017-06-21 10:58:07 -0400

    spagetti original

--- index.js
+++ index.js
@@ -1,60 +1,54 @@
-'use strict';
+$(function(){
+
+  // listen for form submission
+  $('#js-shopping-list-form').submit(function(event) {
+
+    // stop the default form submission behavior
+    event.preventDefault();
+
+    // get the text value for the new item
+    const listItem = $('.js-shopping-list-entry').val();
+
+    // clear out the text input so user can input a new item if they want
+    $('.js-shopping-list-entry').val('');
+
+    // add the new shopping list item as a `li`, following the structure we
+    // already have in `index.html`
+    $('.shopping-list').append(
+      `<li>
+        <span class="shopping-item">${listItem}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>`);
+  });
+
+  // listen for click events on the delete buttons for shopping list items
+  // note that we're using *event delegation* here. since new items can be
+  // added to the shopping list, and we need to listen for delete and check
+  // items on those items, we need to have the event listener on the parent element
+  // which is already present when the page first loads. if this sounds confusing
+  // go back and re-read the discussion of event delegation in the curriculum!
+  $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+
+    // this will look for the closest parent `li` element and remove it from the DOM.
+    // in other words, it will remove the shopping list item.
+    $(this).closest('li').remove();
+  });
 
 
-// `STORE` is responsible for storing the underlying data
-// that our app needs to keep track of in order to work.
-//
-// for a shopping list, our data model is pretty simple.
-// we just have an array of shopping list items. each one
-// is an object with a `name` and a `checked` property that
-// indicates if it's checked off or not.
-// we're pre-adding items to the shopping list so there's
-// something to see when the page first loads.
-const STORE = [
-  {name: "apples", checked: false},
-  {name: "oranges", checked: false},
-  {name: "milk", checked: true},
-  {name: "bread", checked: false}
-];
+  // when a user clicks the "check" button, this will cause the checked off styling
+  // to toggle. as with the delete behavior above, this also uses event delegation.
+  $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
 
+    // go up to the parent li, then from there find the child`.shopping-item`  and
+    // toggle the `.shopping-item__checked` class.
+    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+  });
 
-function renderShoppingList() {
-  // this function will be repsonsible for rendering the shopping list in
-  // the DOM
-  console.log('`renderShoppingList` ran');
-}
-
-
-function handleNewItemSubmit() {
-  // this function will be responsible for when users add a new shopping list item
-  console.log('`handleNewItemSubmit` ran');
-}
-
-
-function handleItemCheckClicked() {
-  // this function will be reponsible for when users click the "check" button on
-  // a shopping list item.
-  console.log('`handleItemCheckClicked` ran');
-}
-
-
-function handleDeleteItemClicked() {
-  // this function will be responsible for when users want to delete a shopping list
-  // item
-  console.log('`handleDeleteItemClicked` ran');
-}
-
-
-// this function will be our callback when the page loads. it's responsible for
-// initially rendering the shopping list, and activating our individual functions
-// that handle new item submission and user clicks on the "check" and "delete" buttons
-// for individual shopping list items.
-function handleShoppingList() {
-  renderShoppingList();
-  handleNewItemSubmit();
-  handleItemCheckClicked();
-  handleDeleteItemClicked();
-}
-
-// when the page loads, call `handleShoppingList`
-$(handleShoppingList);
+});
